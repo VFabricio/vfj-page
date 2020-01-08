b7011498bda9662e8cd1e8658bce1192aacde1c2
Vilson Fabricio Juliatto <vfabricio2201@gmail.com>
2020-01-08 20:09:25 -0300

    Add Nav component.

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,57 +4,34 @@
 
 <style>
 	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	.selected {
-		position: relative;
-		display: inline-block;
-	}
-
-	.selected::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
+		align-items: center;
+		background-color: var(--darkest-blue);
+		display: flex;
+		color: white;
+		height: 35pt;
 	}
 
 	a {
+		font-size: 18pt;
+		padding: 0 0 0 15pt;
 		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
+	}
+
+	.selected {
+		color: var(--svelte-orange);
+	}
+
+	#site-title {
+		border-right: solid white 2px;
+		font-family: "Itim", cursive;
+		font-size: 30pt;
+		font-style: italic;
+		padding: 0 15pt 0 10pt;
 	}
 </style>
 
 <nav>
-	<ul>
-		<li><a class:selected='{segment === undefined}' href='.'>home</a></li>
-		<li><a class:selected='{segment === "about"}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>
-	</ul>
+	<a id="site-title" href="/">Juliatto.dev</a>
+	<a class:selected="{segment==='blog'}" href="blog">Blog</a>
+	<a class:selected="{segment==='projects'}" href="projects">Projects</a>
 </nav>
