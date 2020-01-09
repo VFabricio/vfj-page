2c68c006fd56f26a374a7c6a1e34116e25af72db
Vilson Fabricio Juliatto <vfabricio2201@gmail.com>
2020-01-09 02:45:18 -0300

    Add LeftSideBar component.

---
+++ src/components/LeftSideBar.svelte
@@ -0,0 +1,80 @@
+<style>
+    a {
+        align-items: center;
+        display: flex;
+        font-size: 15px;
+    }
+
+    aside {
+        background-color: var(--lighter-blue);
+        float: left;
+        height: 100%;
+        margin: var(--nav-height) 0 0 0;
+        position: fixed;
+        width: var(--sidebar-width);
+    }
+
+    i {
+        float: left;
+        margin: 0 5px 0 0;
+    }
+
+    img {
+        border-radius: 50%;
+        margin: 20px 0 0 0;
+        width: 120px;
+    }
+
+    p {
+        font-family: 'Itim', cursive;
+        font-size: 18px;
+        font-style: italic;
+        line-height: 14px;
+        margin: 15px 0;
+    }
+
+    section {
+        margin: 0 0 70px var(--left-line);
+    }
+
+    #contact-me {
+        font-family: 'Itim', cursive;
+        font-size: 25px;
+        margin-left: -2.5px;
+    }
+
+    #name {
+        font-size: 32px;
+        font-style: normal;
+        font-weight: bolder;
+        padding: 5px 0;
+    }
+
+</style>
+
+<aside>
+    <section>
+        <img src="vfj-thinking.png" alt="A man thinking.">
+        <p id="name">Fabricio Juliatto</p>
+        <p>Software Developer</p>
+        <p>Byte herder</p>
+        <p>Learner of all things awesome</p>
+    </section>
+
+    <section>
+        <p id="contact-me">Contact Me:</p>
+            <a href="mailto:vfabricio2201@gmail.com">
+                <i class="fas fa-envelope"></i>vfabricio2201@gmail.com
+            </a>
+            <a href="https://twitter.com/vfabricio">
+                <i class="fab fa-twitter"></i>VFabricio
+            </a>
+            
+            <a href="https://github.com/vfabricio">
+                <i class="fab fa-github"></i>VFabricio
+            </a>
+            <a href="https://linkedin.com/in/vilson-fabricio-juliatto">
+                <i class="fab fa-linkedin"></i>vilson-fabricio-juliatto
+            </a>
+    </section>
+</aside>
