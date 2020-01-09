a2e067b37aa7e89ade5598d8a87e470f37013f28
Vilson Fabricio Juliatto <vfabricio2201@gmail.com>
2020-01-09 02:43:51 -0300

    Position the 'main' element on the layout file.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,6 +5,14 @@
 	export let segment;
 </script>
 
+<style>
+	main {
+		--extra-padding: 30px;
+		padding-top: calc(var(--nav-height) + var(--extra-padding));
+		padding-left: calc(var(--sidebar-width) + var(--extra-padding));
+	}
+</style>
+
 <Nav {segment}/>
 
 <LeftSideBar/>
